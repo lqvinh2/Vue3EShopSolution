c247c6264abd2c5ca3718272c2b90c20d14e9601
qmloffice <lqvinh.hsu@gmail.com>
2021-05-19 23:57:11 +0700

    learning Vuex4  2.0 STATE
    learning Vuex4  2.0 STATE

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,40 +1,94 @@
 <template>
-<h2>My App TODOS , now you have : {{projects.length}} TODO job</h2>
+  <h2>My App TODOS , now you have : {{ projects.length }} TODO job</h2>
+  <h2>{{ count }}</h2>
+  <div class="input-group mb-3">
+    <input
+      v-model="newNum"
+      type="number"
+      class="form-control"
+      placeholder="num"
+      aria-label="num"
+      aria-describedby="basic-addon1"
+    />
+  </div>
+  <button
+    @click="incrementCount(parseInt(newNum))"
+    type="button"
+    class="btn btn-success"
+  >
+    increment Count
+  </button>
 
+  <button
+    @click="addRandomNumberAxios"
+    type="button"
+    class="btn btn-success"
+    :disabled="false"
+  >
+    Add random number
+  </button>
+
+  <input placeholder="Search by Index" type="number" v-model="newNum" />
+  <div class="showNumList">
+    <p
+      v-for="(number, index) in history"
+      :key="index"
+      :class="
+        getIndexAfterAddNumberToHistory(parseInt(newNum)).includes(index) &&
+          'bold'
+      "
+    >
+      {{ number }}
+    </p>
+  </div>
 </template>
 
 <script>
-
-import { computed, watch, ref , onMounted } from 'vue';
-import { useRouter } from 'vue-router';
-import {mapState} from 'vuex'
+import { computed, watch, ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import { mapState, mapMutations, mapActions, mapGetters } from "vuex";
 ////
 // import Todos from '../src/components/Todos.vue'
 export default {
-    name:'Navbar',
-    props: ['',''],
-    // components:{Todos},
-    computed : {
-        ...mapState(["projects"])
-    },
-    setup() {
-        onMounted(() => {
-        
-        });
-        const router = useRouter();
-        
-        // code here
-        const emailXXX = ref('');
-        const handleSubmit = () => {
-            console.log('1234');
-        };
-        ///////////////
-        
-        return { emailXXX , handleSubmit};
-    },
-}
+  name: "Navbar",
+  props: ["", ""],
+  // components:{Todos},
+  computed: {
+    ...mapState(["projects", "count", "history"]),
+    ...mapGetters(["getIndexAfterAddNumberToHistory"]),
+  },
+
+  methods: {
+    ...mapMutations(["incrementCount"]),
+    ...mapActions(["addRandomNumberAxios"]),
+  },
+  setup() {
+    onMounted(() => {});
+    const router = useRouter();
+
+    // code here
+    const emailXXX = ref("");
+    const handleSubmit = () => {
+      console.log("1234");
+    };
+    const newNum = ref(100);
+    ///////////////
+
+    return { emailXXX, handleSubmit, newNum };
+  },
+};
 </script>
 
-<style>
+<style scoped>
 /* @import 'assets/css/main.css'; */
+.bold {
+  font-weight: 900;
+}
+.showNumList {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+
+  width: 100%;
+}
 </style>
