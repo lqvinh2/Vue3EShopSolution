f910f317e71550753ae2114b99b750424be6c32b
lqvinh2 <lqvinh.hsu@gmail.com>
2021-05-21 17:54:36 +0700

    create navBar v1
    create navBar v1

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+
+
 import UseStoreNavbar from "./Store/UseStoreNavbar.js";
 
 import "./assets/js/developer/dev_main.js";
@@ -9,10 +11,7 @@ import helmet from "helmet";
 
 const app = createApp(App);
 app.use(helmet);
-
 app.use(router);
-
-//const { storeNavbar } = UseStoreNavbar();
 app.use(UseStoreNavbar);
-
+//
 app.mount("#app");
